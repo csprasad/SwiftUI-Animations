6a0e8bc7cbadd89bbe0d6b9e8cb32734de11f0af
CSPrasad <csprasad195@gmail.com>
2023-07-20 18:06:53 +0530

    Resource update.

---
+++ SwiftUI-Animations/Views/Animations/RocketLaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  RocketLaunchView.swift
+//  SwiftUI-Animations
+//
+//  Created by CS Prasad on 20/07/23.
+//
+
+import SwiftUI
+
+struct RocketLaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RocketLaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        RocketLaunchView()
+    }
+}
