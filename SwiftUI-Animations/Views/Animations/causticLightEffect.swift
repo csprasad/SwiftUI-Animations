512769550146e28255acf68d3ec4eb90973a8e8b
csprasad <csprasad195@gmail.com>
2024-08-08 16:08:06 +0530

    New Animation added octopus movement

--- SwiftUI-Animations/Views/Animations/causticLightEffect.swift
+++ SwiftUI-Animations/Views/Animations/causticLightEffect.swift
@@ -8,44 +8,66 @@
 import SwiftUI
 
 struct causticLightEffect: View {
-    @State private var animate = false
-    let circleCount = 10
-    let layerCount = 10
-    let maxRadius: CGFloat = 150
+    @State private var move = false
 
     var body: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
             
-            ForEach(0..<layerCount, id: \.self) { layer in
-                ForEach(0..<circleCount, id: \.self) { index in
-                    Circle()
-                        .fill(Color.white)
-                        .frame(width: 10, height: 10)
-                        .offset(x: animate ? offset(for: index, in: layer).x + 100 : 0,
-                                y: animate ? offset(for: index, in: layer).y - 300 : 0)
-                        .animation(
-                            Animation.linear(duration: 5)
-                                .repeatForever(autoreverses: true)
-                                .delay(Double(index + layer * circleCount) * 0.02)
-                        )
-                }
-            }
+            // Layer 1
+            LinearGradient(
+                gradient: Gradient(colors: [.white.opacity(0.2), .clear]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .mask(
+                CausticPattern()
+                    .scaleEffect(x: 1.5, y: 1.5)
+            )
+            .rotationEffect(.degrees(move ? 360 : 0))
+            .animation(
+                Animation.linear(duration: 10)
+                    .repeatForever(autoreverses: false),
+                value: move
+            )
+            
+            // Layer 2
+            LinearGradient(
+                gradient: Gradient(colors: [.clear, .white.opacity(0.2)]),
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+            .mask(
+                CausticPattern()
+                    .scaleEffect(x: 1.5, y: 1.5)
+            )
+            .rotationEffect(.degrees(move ? -360 : 0))
+            .animation(
+                Animation.linear(duration: 12)
+                    .repeatForever(autoreverses: false),
+                value: move
+            )
         }
         .onAppear {
-            animate = true
+            move = true
         }
     }
+}
 
-    func offset(for index: Int, in layer: Int) -> CGPoint {
-        let angle = 2 * .pi / Double(circleCount) * Double(index)
-        let radius = maxRadius / CGFloat(layerCount) * CGFloat(layer + 1)
-        let x = cos(angle) * radius
-        let y = sin(angle) * radius
-        return CGPoint(x: x, y: y)
+struct CausticPattern: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // Draw a custom pattern resembling caustics
+        for i in stride(from: 0, to: rect.width, by: rect.width / 10) {
+            let xOffset = CGFloat.random(in: -20...20)
+            let yOffset = CGFloat.random(in: -20...20)
+            path.addEllipse(in: CGRect(x: i + xOffset, y: i + yOffset, width: 100, height: 100))
+        }
+        
+        return path
     }
 }
-    
 
 
 
